3c0a5527878b316c682f260ebb69b66d4174485f
Andrew Angelle <andrewangelle@Andrews-MacBook-Air.local>
2021-12-26 17:07:42 -0500

    add some stuff

--- src/components/WebSynth.tsx
+++ src/components/WebSynth.tsx
@@ -1,7 +1,12 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import AudioContext from 'audio-context';
 
-function useOscillator(){
+function useOscillator({
+  filterValue
+}: {
+  filterValue: number;
+}): [OscillatorNode, BiquadFilterNode, GainNode, ]{
+
   const audioContext = AudioContext()!; 
 
   const context = useRef(audioContext);
@@ -9,24 +14,59 @@ function useOscillator(){
   const filter = context.current.createBiquadFilter()!;
   const gain = context.current.createGain()!;
 
+
   oscillator
     .connect(filter)
     .connect(gain)
     .connect(audioContext.destination)
 
-      
 
-  return oscillator
+  filter.frequency.value = filterValue
+
+
+  return [oscillator, filter, gain]
 
 }
 
 export function WebSynth(){
-  const oscillator = useOscillator()  
+  const [isPlaying, setPlaying] = useState(false)
+  const [filterValue, setFilterValue] = useState(1)
+  const [oscillator, filter, gain] = useOscillator({
+    filterValue
+  })
+
+  filter.type = 'lowpass'
   return (
-    <div>
-      <button onClick={() => oscillator.start()}>
+    <div
+      style={{display: 'flex', flexDirection: 'column'}}
+    >
+      <button 
+        style={{width: '100px', margin: 'auto'}}
+        onClick={() => oscillator.start()}
+      >
         Start Synth
       </button>
+
+      <button 
+        style={{width: '100px', margin: '10px auto'}}
+        onClick={() => oscillator.stop()}
+      >
+        Stop Synth
+      </button>   
+
+      <label
+        style={{width: '100px', margin: '10px auto auto'}} 
+        htmlFor="filter"
+      >
+        Filter
+      </label>
+      <input 
+        name="filter"
+        type='range'
+        style={{width: '50%', margin: 'auto'}} 
+        value={filterValue.toString()}
+        onChange={(e) => setFilterValue(Number(e.target.value))} 
+      />
     </div>
   ) 
 }
